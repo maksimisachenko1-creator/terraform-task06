9b4091a4a2220261fb42d0e6aee92add57343e8d
Maksim Isachenko <maksim_isachenko1@epam.com>
2025-10-16 19:27:05 +0400

    fixes 1

--- task06/variables.tf
+++ task06/variables.tf
@@ -11,23 +11,27 @@ variable "name_prefix" {
 }
 
 variable "existing_key_vault_resource_group" {
-  type    = string
-  default = "cmaz-ya42kk4d-mod6-kv-rg"
+  description = "Resource group of an existing Key Vault to store SQL admin password"
+  type        = string
+  default     = "cmaz-ya42kk4d-mod6-kv-rg"
 }
 
 variable "existing_key_vault_name" {
-  type    = string
-  default = "cmaz-ya42kk4d-mod6-kv"
+  description = "Name of an existing Key Vault to store SQL admin password"
+  type        = string
+  default     = "cmaz-ya42kk4d-mod6-kv"
 }
 
 variable "rg_name" {
-  type    = string
-  default = "cmaz-ya42kk4d-mod6-rg"
+  description = "Resource group name"
+  type        = string
+  default     = "cmaz-ya42kk4d-mod6-rg"
 }
 
 variable "sql_admin_username" {
-  type    = string
-  default = "sqladmin"
+  description = "SQL admin username"
+  type        = string
+  default     = "sqladmin"
 }
 
 variable "allowed_ip_address" {
@@ -37,6 +41,7 @@ variable "allowed_ip_address" {
 }
 
 variable "tags" {
-  type    = map(string)
-  default = { Creator = "maksim_isachenko1@epam.com" }
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = { Creator = "maksim_isachenko1@epam.com" }
 }
